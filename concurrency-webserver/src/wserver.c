ac456148194f12f17532b2062015a7ebf020bb9a
fathom820 <michaelfrank2002@gmail.com>
2022-04-04 15:06:13 -0400

    dhshsrth

--- concurrency-webserver/src/wserver.c
+++ concurrency-webserver/src/wserver.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <pthread.h>
+
 #include "request.h"
 #include "io_helper.h"
 
 char default_root[] = ".";
+
+void *handle(int fd) {
+	request_handle(fd);
+	printf("child: done\n");
+	return NULL;
+}
 
 //
 // ./wserver [-d <basedir>] [-p <portnum>] 
@@ -13,17 +22,17 @@ int main(int argc, char *argv[]) {
     int port = 10000;
     
     while ((c = getopt(argc, argv, "d:p:")) != -1)
-        switch (c) {
-        case 'd':
-            root_dir = optarg;
-            break;
-        case 'p':
-            port = atoi(optarg);
-            break;
-        default:
-            fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
-            exit(1);
-        }
+	switch (c) {
+	case 'd':
+	    root_dir = optarg;
+	    break;
+	case 'p':
+	    port = atoi(optarg);
+	    break;
+	default:
+	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
+	    exit(1);
+	}
 
     // run out of this directory
     chdir_or_die(root_dir);
@@ -31,11 +40,11 @@ int main(int argc, char *argv[]) {
     // now, get to work
     int listen_fd = open_listen_fd_or_die(port);
     while (1) {
-        struct sockaddr_in client_addr;
-        int client_len = sizeof(client_addr);
-        int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-        request_handle(conn_fd);
-        close_or_die(conn_fd);
+	struct sockaddr_in client_addr;
+	int client_len = sizeof(client_addr);
+	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
+	close_or_die(conn_fd);
+	//TODO: create thread
     }
     return 0;
 }
