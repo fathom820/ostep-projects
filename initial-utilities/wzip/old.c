dc062e0388cf60117c5a2e8e34a7b9783c3516e2
Michael Frank <michaelfrank2002@gmail.com>
2022-04-15 16:20:22 -0400

    logic refactor

--- initial-utilities/wzip/old.c
+++ initial-utilities/wzip/old.c
@@ -29,7 +29,7 @@ int main (int argc, char *argv[]){
                 printf("%c", end);
                 end = c;
                 char_count = 2;
-            }
+            }     
         }  
         fclose(fp); 
     }
--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -2,22 +2,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 
 void write_(int count, char last) {
-    fwrite(&count, sizeof(int), 1, stdout);
-    printf("%c", last);
+    fwrite(&count, sizeof(int), 1, stdout); // write 4-bit integer address to file
+    printf("%c", last);                     // write char to file
 }
 
 int main (int argc, char *argv[]) {
     // needs at least 1 argument
-    if (argc <= 1) {
-        printf("wzip: file1 [file2 ...]\n");
+    if (argc == 1) {
+        printf("wzip: file1 [file2 ...]");
         exit(1);
     }
 
-    int count = 0;                  // char counter
-    char chr_current = '\0';        // current char in string, null term used as default value because it will never occur in file
-    char chr_last = chr_current;        // "current" char that is being counted
+    bool first  = false;    // boolean to test if first byte has been read yet
+    int count   = 0;        // char counter
+    char chr;               // "actual" current char in string, null term used as default value because it will never occur in file
+    char last;              // "current" char that is being counted -- is compared to chr_current
+
     
     for (int i = 1; i < argc; i++) {
         FILE *fp = fopen(argv[i], "r");
@@ -26,35 +29,33 @@ int main (int argc, char *argv[]) {
             printf("wzip: unable to open file\n");
             exit(1);
         }
-    
-        while(1) {
-            chr_current = fgetc(fp);
-            // exit at end of file
-            if (chr_current == EOF) {
-                break;
-            }
-
-            // initial condition
-            if (chr_last == '\0') {
-                chr_last = chr_current;
-                count = 1;
+            
+        while(chr = fgetc(fp) != EOF) {
+            // initial condition:
+            // for the first character, there is 
+            // no previous character to refer to,
+            // we must handle it independently 
+            if (!first) {
+                last    = chr;
+                count   = 1;
+                first   = true;
             }
 
             // add to count of current character
-            else if (chr_last == chr_current) {
+            else if (last == chr) {
                 ++count;
             }
 
-            // if character changes
+            // if character changes, make it the new
+            // character to be compared to the others
             else {
-                write_(count, chr_last);
-                chr_last = chr_current;
-                count = 1;
+                write_(count, last);
+                last    = chr;
+                count   = 1;
             }
         }
         fclose(fp);
     }
-
-    write_(count, chr_last);
+    write_(count, last);
     return 0;
 }
