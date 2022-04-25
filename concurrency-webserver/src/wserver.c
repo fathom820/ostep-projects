8ee5a8ff94063182a33d57b659ff578311fc519a
Michael Frank <michaelfrank2002@gmail.com>
2022-04-24 23:40:29 -0400

    refactor, prep for multi-thread, test6 now fails

--- concurrency-webserver/src/wserver.c
+++ concurrency-webserver/src/wserver.c
@@ -1,79 +1,97 @@
 #include <stdio.h>
 #include <pthread.h>
+
 #include "request.h"
 #include "io_helper.h"
+#include "thread-pool.h"
+#include "queue.h"
+
+#define DEBUG 1
+
 
 char default_root[] = ".";
 
 void *mythread(int fd) {
-  printf("opening thread\n");
+  msg("opening thread");
   request_handle(fd);
   close_or_die(fd);
   return NULL;
 }
 
+void msg(char * m) {
+  if (DEBUG == 1) {
+    printf("%s\n", m);
+  }
+}
+
 //
 // ./wserver [-d <basedir>] [-p <portnum>] 
 // 
-int main(int argc, char *argv[]) {
-  int c;
-  char *root_dir  = default_root;
-  int port        = 10000;
-  int threads     = 1;
-  int buffers     = 1;
-  char * schedalg = "FIFO";
+// int main(int argc, char *argv[]) {
+//   int c;
+//   char *root_dir  = default_root;
+//   int port        = 10000;
+//   int threads     = 1;
+//   int buffers     = 1;
+//   char * schedalg = "FIFO";
     
-  while ((c = getopt(argc, argv, "d:p:t:b:s:")) != -1)
-    switch (c) {
-      case 'd':
-        root_dir = optarg;
-        printf("d: %s\n", root_dir);
-        break;
-      case 'p':
-        port = atoi(optarg);
-        printf("p: %d\n", port);
-        break;
-      case 't':
-        threads = atoi(optarg);
-        printf("t: %d\n", threads);
-        break;
-      case 'b':
-        buffers = atoi(optarg);
-        printf("b: %d\n", buffers);
-        break;
-      case 's':
-        if (strcmp(optarg, "FIFO") == 0 || strcmp(optarg, "SFF") == 0) {
-          schedalg = optarg;
-          printf("s: %s\n", schedalg);
-        } else {
-          fprintf(stderr, "scheduling alg must be \"FIFO\" or \"SFF\"\n");
-          exit(1);
-        }
-        break;
-      default:
-        fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s scheduling algorithm]\n");
-        exit(1);
-    }
+//   while ((c = getopt(argc, argv, "d:p:t:b:s:")) != -1)
+//     switch (c) {
+//       case 'd':
+//         root_dir = optarg;
+//         // printf("d: %s\n", root_dir);
+//         break;
+//       case 'p':
+//         port = atoi(optarg);
+//         // printf("p: %d\n", port);
+//         break;
+//       case 't':
+//         threads = atoi(optarg);
+//         // printf("t: %d\n", threads);
+//         break;
+//       case 'b':
+//         buffers = atoi(optarg);
+//         // printf("b: %d\n", buffers);
+//         break;
+//       case 's':
+//         if (strcmp(optarg, "FIFO") == 0 || strcmp(optarg, "SFF") == 0) {
+//           schedalg = optarg;
+//           // printf("s: %s\n", schedalg);
+//         } else {
+//           fprintf(stderr, "scheduling alg must be \"FIFO\" or \"SFF\"\n");
+//           exit(1);
+//         }
+//         break;
+//       default:
+//         fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers] [-s scheduling algorithm]\n");
+//         exit(1);
+//     }
 
-  //printf("d: %s, p: %d, t: %d, b: %d, s: %s", root_dir, port, threads, buffers, schedalg);
-  // run out of this directory
-  chdir_or_die(root_dir);
+//   printf("d: %s, p: %d, t: %d, b: %d, s: %s\n", root_dir, port, threads, buffers, schedalg);
+//   // run out of this directory
+//   chdir_or_die(root_dir);
 
-  // now, get to work
-  int listen_fd = open_listen_fd_or_die(port);
+//   // now, get to work
+//   msg("get to work");
+//   int listen_fd = open_listen_fd_or_die(port);
+//   msg("create listen_fd");
+//   thread_pool_init(threads);
 
-  while (1) {
-    pthread_t p1;
+//   while (1) {
+//     msg("top of loop");
+//     pthread_t p1;
 
-    struct sockaddr_in client_addr;
-    int client_len = sizeof(client_addr);
-    int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-
-    if (pthread_create(&p1, NULL, request_handle, conn_fd) != 0) {
-      printf("Thread failure\n");
-    } else {
-      printf("Thread success\n");
-    }
-  }
-  return 0;
-}
+//     struct sockaddr_in client_addr;
+//     int client_len = sizeof(client_addr);
+//     msg("client variables");
+//     printf("listen_fd: %d\n", listen_fd);
+//     // int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
+    
+//     // if (pthread_create(&p1, NULL, request_handle, (void*)conn_fd) != 0) {
+//     //   printf("Thread failure\n");
+//     // } else {
+//     //   printf("Thread success\n");
+//     // }
+//   }
+//   return 0;
+// }
--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -3,59 +3,144 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
+
+// ! assumes file has already been opened
 void write_(int count, char last) {
-    fwrite(&count, sizeof(int), 1, stdout); // write 4-bit integer address to file
-    printf("%c", last);                     // write char to file
+  fwrite(&count, sizeof(int), 1, stdout); // write 4-bit integer address to file
+  printf("%c", last);                     // write char to file
 }
 
 int main (int argc, char *argv[]) {
-    // needs at least 1 argument
-    if (argc == 1) {
-        printf("wzip: file1 [file2 ...]");
-        exit(1);
+  if (argc == 1) {
+    printf("wzip: file1 [file2 ...]\n");
+    exit(1);
+  }
+
+
+  // * var init
+  // ? might need to move this block to lesser scope inside (file_size <= 4096)
+  struct stat st;     // stat for measuring file size (from linux lib sys/stat.h)
+  int file_size;      // size of current file (determines which algorithm)
+
+  bool first_read = false; // if first byte has been read (special case)
+  int count = 0;      // counter for chr_last
+  char chr_current;   // current char being compared to chr_last
+  char chr_last;      // current char being counted
+
+
+  // * for each file ... 
+  for (int i = 1; i < argc; i++) {
+    // * open file, test if valid; exit program if not
+    FILE *fp = fopen(argv[i], "r");
+    if (!fp) {
+      printf("wzip: unable to open file\n");
+      exit(1);
     }
 
-    bool first  = false;    // boolean to test if first byte has been read yet
-    int count   = 0;        // char counter
-    char chr;               // "actual" current char in string, null term used as default value because it will never occur in file
-    char last;              // "current" char that is being counted -- is compared to chr_current
+    // * get file size
+    stat(argv[i], &st);
+    file_size = st.st_size;
 
-    
-    for (int i = 1; i < argc; i++) {
-        FILE *fp = fopen(argv[i], "r");
-    
-        if (!fp) {
+    // * determine compression algorithm based on file size
+    if (file_size <= 4096) { // p2s1 compression (non-threaded)
+
+      while(1) {
+        // move to next char, exit at EOF
+        if((chr_current = fgetc(fp)) == EOF) break;
+
+        // * initial condition:
+        // for the first character, there is
+        // no previous character to refer to;
+        // we must handle it independently
+        if (!first_read) {
+          chr_last = chr_current;
+          count = 1;
+          first_read = true;
+        }
+
+        // * add to count of current character
+        else if (chr_last == chr_current) {
+          ++count;
+        }
+
+        // * if character changes, make it the new
+        // * character to be compared to the others
+        else {
+          write_(count, chr_last);
+          chr_last = chr_current;
+          count = 1;
+        }
+      }
+      fclose(fp);
+    }
+  }
+  write_(count, chr_last);
+  return 0;
+}
+
+
+
+
+
+int mainold (int argc, char *argv[]) {
+  // needs at least 1 argument
+  switch (argc) {
+    case 1:
+      printf("wzip: file1 [file2 ...]\n");
+      exit(1);
+
+    default:
+      {   
+        bool first = false;     // boolean to test if first byte has been read yet
+        int count = 0;          // char counter
+        char chr_current;       // current char in string, null term used as default value because it will never occur in file
+        char chr_last;          // "current" char that is being counted -- is compared to chr_current
+
+        for (int i = 1; i < argc; i++) {
+          FILE *fp = fopen(argv[i], "r");
+
+          if (!fp) {
             printf("wzip: unable to open file\n");
             exit(1);
-        }
-            
-        while(chr = fgetc(fp) != EOF) {
+          }
+
+          while(1) {
+            chr_current = fgetc(fp);
+
+            // exit at end of file
+            if (chr_current == EOF) {
+              break;
+            }
+
             // initial condition:
             // for the first character, there is 
             // no previous character to refer to,
             // we must handle it independently 
             if (!first) {
-                last    = chr;
-                count   = 1;
-                first   = true;
+              chr_last = chr_current;
+              count = 1;
+              first = true;
             }
 
             // add to count of current character
-            else if (last == chr) {
-                ++count;
+            else if (chr_last == chr_current) {
+              ++count;
             }
 
             // if character changes, make it the new
             // character to be compared to the others
             else {
-                write_(count, last);
-                last    = chr;
-                count   = 1;
+              write_(count, chr_last);
+              chr_last = chr_current;
+              count = 1;
             }
+          }
+          fclose(fp);
         }
-        fclose(fp);
-    }
-    write_(count, last);
-    return 0;
+        write_(count, chr_last);
+        return 0;
+      }
+  }
 }
